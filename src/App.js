9f844ccc8e8907bc2d05165792280f61622a882b
Faruq Adelodun <horlayeanca@192-80-0-84.cab.webpass.net>
2023-05-29 04:09:36 +0100

    added react-router-dom and custom font.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Hero from './components/Hero';
 import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div>
     <Navigation />
     <Hero />
     </div>
+    </Router>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,64 +1,109 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faScissors, faTimes } from '@fortawesome/free-solid-svg-icons'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faScissors, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function Navigation() {
-    const [showMenu, setShowMenu] = React.useState(false)
-    const toggleMenu = () => setShowMenu(!showMenu)
-    
-    const menuLinks = [
-        {
-            id: 1,
-            link: 'My URLs',
-        },
-        {
-            id: 2,
-            link: 'Features',
-        },
-        {
-            id: 3,
-            link: 'Pricing',
-        },
-        {
-            id: 4,
-            link: 'Analytics',
-        },
-        {
-            id: 5,
-            link: 'FAQs',
-        },
-    ]
-  return (
-    <div className='flex justify-between items-center w-full h-20 px-20 gap-9'>
-    <div>
-    <h1 className='text-2xl text-blue-600 font-bold'><FontAwesomeIcon icon={faScissors} size="2xs" style={{color: "#0065fe",}} /> | scissor</h1>
-    </div>
-    <div className='hidden md:flex'>
-    {menuLinks.map(({id, link}) => (
-        <li key={id} className='px-4 cursor-pointer list-none capitalize text-lg text-black hover:scale-105 duration-300 hover:text-blue-600'>{link}</li>
-    ))}
-    </div>
-    <div className='sm:flex justify-center items-center gap-3 hidden'>
-    <button className='text-blue-600 text-base font-medium cursor-pointer leading-5 w-20 hover:scale-100 duration-300 hover:text-red-400'>Log in</button>
-    <button className='flex flex-row justify-center cursor-pointer items-center py-3 px-6 bg-blue-600 rounded-full text-white w-36 h-11 hover:scale-100 duration-300 hover:text-blue-600 hover:bg-red-400'>Try for free</button>
-    </div>
-    <div onClick={() => setShowMenu(!showMenu)} className='pr-4 z-10 text-blue-600 md:hidden'>
-    {showMenu ? <FontAwesomeIcon icon={faTimes} size="xl" style={{color: "#0065fe",}} className='px-1'/> : <FontAwesomeIcon icon={faBars} size="xl" style={{color: "#0065fe",}} className='px-1'/>}
+  const [showMenu, setShowMenu] = React.useState(false);
+  const toggleMenu = () => setShowMenu(!showMenu);
 
+  const menuLinks = [
+    {
+      id: 1,
+      link: "My URLs",
+    },
+    {
+      id: 2,
+      link: "Features",
+    },
+    {
+      id: 3,
+      link: "Pricing",
+    },
+    {
+      id: 4,
+      link: "Analytics",
+    },
+    {
+      id: 5,
+      link: "FAQs",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-20 gap-9">
+      <div>
+        <h1 className="text-2xl text-blue-600 font-extrabold cursor-pointer font-gilroy">
+          <FontAwesomeIcon
+            icon={faScissors}
+            size="2xs"
+            style={{ color: "#0065fe" }}
+          />{" "}
+          | scissor
+        </h1>
+      </div>
+      <div className="hidden md:flex">
+        {menuLinks.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 list-none capitalize font-gilroy font-light text-lg text-black cursor-pointer hover:scale-100 duration-300 hover:text-red-400"
+          >
+          <Link to={link}>{link}</Link>
+          </li>
+        ))}
+      </div>
+      <div className="md:flex justify-center items-center gap-3 hidden">
+        <button className="text-blue-600 text-base font-medium cursor-pointer leading-5 w-20 hover:scale-100 duration-300 hover:text-red-400">
+          Log in
+        </button>
+        <button className="flex flex-row justify-center cursor-pointer items-center py-3 px-6 bg-blue-600 rounded-full text-white w-36 h-11 hover:scale-100 duration-300 hover:text-blue-600 hover:bg-red-400">
+          Try for free
+        </button>
+      </div>
+      <div onClick={toggleMenu} className="pr-4 z-10 text-blue-600 md:hidden">
+        {showMenu ? (
+          <FontAwesomeIcon
+            icon={faTimes}
+            size="xl"
+            
+            className="px-1"
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faBars}
+            size="xl"
+            
+            className="px-1"
+          />
+        )}
+      </div>
+
+      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen text-black">
+        {showMenu &&
+          menuLinks.map(({ id, link }) => (
+            <li
+              key={id}
+              onClick={toggleMenu}
+              className="px-4 cursor-pointer list-none capitalize font-medium py-5 text-xl text-black"
+            >
+              <Link to={link}>{link}</Link>
+            </li>
+          ))}
+        <div className="flex flex-col justify-center items-center gap-3 mt-4 bg-white w-full">
+          {showMenu && (
+            <button className="text-blue-600 text-base font-medium leading-5 w-20 hover:scale-100 duration-300 hover:text-red-400">
+              Log in
+            </button>
+          )}
+          {showMenu && (
+            <button className="flex flex-row justify-center items-center py-3 px-6 bg-blue-600 rounded-full text-white w-36 h-11 hover:scale-100 duration-300 hover:text-blue-600 hover:bg-red-400">
+              Try for free
+            </button>
+          )}
+        </div>
+      </ul>
     </div>
-    <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen text-black'>
-    { showMenu && menuLinks.map(({id, link}) => (
-        <li key={id} onClick={toggleMenu} className='px-4 cursor-pointer list-none capitalize font-medium text-black'>{link}</li>
-        
-    ))}
-    <div className='flex flex-col justify-center items-center gap-3 mt-4'>
-    {showMenu && <button className='text-blue-600 text-base font-medium leading-5 w-20 hover:scale-100 duration-300 hover:text-red-400'>Log in</button>}
-    {showMenu && <button className='flex flex-row justify-center items-center py-3 px-6 bg-blue-600 rounded-full text-white w-36 h-11 hover:scale-100 duration-300 hover:text-blue-600 hover:bg-red-400'>Try for free</button>}
-    </div>
-    </ul>
-    </div>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
