f107b1486806b5d98653b0b461d719b8826c1516
Faruq Adelodun <horlayeanca@192-80-3-79.cab.webpass.net>
2023-06-04 22:04:17 +0100

    refactored Shortner.js

--- src/components/Shortner.js
+++ src/components/Shortner.js
@@ -1,10 +1,52 @@
 import React from "react";
-import { ReactComponent as BackgroundI } from "../assets/BackgroundI.svg";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandScissors } from "@fortawesome/free-solid-svg-icons";
 
 function Shortner() {
   return (
-    <div>
-      <BackgroundI />
+    <div className="flex justify-center items-center bg-gradient-to-br from-secondary to-blue-800 py-20">
+      <div className="border-2 rounded-xl flex flex-col justify-center items-center p-11 gap-5 w-1/3 bg-white">
+        <form action="" className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Paste URL here..."
+            className="border-2 border-blue-200 rounded-xl w-96 px-4 py-2 font-gilroy focus:outline-blue-400"
+          />
+          <div className="flex gap-2">
+            <button
+              type="select"
+              className="border-2 border-blue-200 px-4 py-2 rounded-xl font-gilroy text-sm text-blue-400"
+            >
+              Customize domain &nbsp;
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                style={{ color: "#3284ff" }}
+              />
+            </button>
+            <input
+              type="text"
+              placeholder="Type Alias here"
+              className="border-2 border-blue-200 px-3 py-2 rounded-xl font-gilroy focus:outline-blue-400"
+            />
+          </div>
+          <button className="bg-blue-600 py-2 w-full rounded-full text-white font-gilroy font-semibold text-sm hover:bg-gradient-to-br from-green-300 to-blue-400 duration-200">
+            Trim URL &nbsp;{" "}
+            <FontAwesomeIcon
+              icon={faHandScissors}
+              style={{ color: "#f4f5f5" }}
+            />
+          </button>
+          <p className="font-gilroy text-blue-500 text-sm font-medium text-center ">
+            By clicking Trim URL, I agree to the{" "}
+            <span className="font-bold text-base text-blue-600">
+              {" "}
+              Terms of Service, Privacy Policy
+            </span>{" "}
+            and use of Cookies.
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         white: "#F9FBFD",
         primary: "#005AE2",
         secondary: "#1E3448",
+        linear:
+          "linear-gradient(180deg, #1E3448 99.99%, rgba(30, 52, 72, 0) 100%)",
       },
     },
     plugins: [],
