d2f66e7cac40825e8b36b9e359c541421ee1c0eb
horlayeanca <adelodunfaruq@gmail.com>
2023-06-09 06:45:09 +0100

    refactoring GetStarted.js

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { ReactComponent as VectorVIII } from "../assets/VectorVIII.svg";
+import { ReactComponent as VectorIX } from "../assets/VectorIX.svg";
+import { ReactComponent as VectorX } from "../assets/VectorX.svg";
+import { ReactComponent as VectorXI } from "../assets/VectorXI.svg";
+import { ReactComponent as VectorXII } from "../assets/VectorXII.svg";
+import { ReactComponent as VectorXIII } from "../assets/VectorXIII.svg";
 
 function GetStarted() {
   return (
-    <div>
-      <div>
-        <h1>Revolutionizing Link Optimization</h1>
-        <button>Get Started</button>
+    <div className="pt-20 w-full">
+      <div className="bg-secondary">
+        <VectorVIII className="absolute -left-1" />
+        <VectorIX className="absolute -left-2" />
+        <VectorX className="absolute -left-1" />
+        <div className="flex justify-center items-center flex-col">
+          <h1 className="font-gilroy text-4xl font-bold text-white pb-10">
+            Revolutionizing Link Optimization
+          </h1>
+          <button className="bg-primary py-3 px-10 rounded-full text-white font-gilroy font-normal text-base hover:bg-gradient-to-br from-green-300 to-blue-400 duration-300 hover:text-black ">
+            Get Started
+          </button>
+          <VectorXI className="absolute right-1" />
+          <VectorXII className="absolute right-2" />
+          <VectorXIII className="absolute right-1 " />
+        </div>
       </div>
     </div>
   );
