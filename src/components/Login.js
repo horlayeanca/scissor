27c3802068c07c4b90465a01ce5811802d82cf18
horlayeanca <adelodunfaruq@gmail.com>
2023-06-12 20:13:24 +0100

    refactored login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ function Login() {
     <div className="w-full h-[714px] bg-[#ffffff] flex justify-center">
       <div className="flex justify-center items-center flex-col border-2 border-black w-1/2">
         <h3>Log in with:</h3>
-        <div className="flex items-center border-2 border-red-300 font-gilroy text-white">
+        <div className="flex items-center font-gilroy text-white py-3">
           <button className="bg-blue-600 py-2 px-5 flex items-center rounded">
             <Google className="w-4 h-4 flex" /> &nbsp; Google
           </button>
@@ -24,39 +24,50 @@ function Login() {
           <VectorXX /> <h2>&nbsp; Or &nbsp;</h2>
           <VectorXX />
         </div>
-        <form action="" className="flex flex-col">
+        <form action="" className="flex flex-col py-2">
           <input
             type="email"
             name=""
             id=""
             placeholder="Email address or username"
-            className="border-2 border-blue-600 rounded-lg w-[450px] py-2 px-4"
-          />
+            className="border-2 border-blue-600 rounded-lg w-[500px] py-2 px-4"
+          />{" "}
+          <br />
           <input
             type="password"
             name=""
             id=""
             placeholder="Password"
-            className="border-2 border-blue-600 rounded-lg w-[450px] py-2 px-4"
+            className="border-2 border-blue-600 rounded-lg w-[500px] py-2 px-4"
           />
-          <FontAwesomeIcon icon={faEyeSlash} />
-          <FontAwesomeIcon icon={faEye} />
-          <h2 className="font-gilroy text-right">Forgot password ?</h2>
-          <button className="bg-blue-600 py-2 rounded-full font-gilroy">
+          <FontAwesomeIcon
+            icon={faEyeSlash}
+            className="absolute right-[450px] mt-[82px] cursor-pointer hidden"
+          />
+          <FontAwesomeIcon
+            icon={faEye}
+            className="absolute right-[450px] mt-[82px] cursor-pointer"
+          />
+          <h2 className="font-gilroy text-right py-2 text-blue-600 hover:text-blue-500">
+            <a href="/#">Forgot password ?</a>
+          </h2>
+          <button className="bg-blue-600 py-3 text-sm font-semibold rounded-full font-gilroy text-white hover:bg-blue-500">
             Log in
           </button>
-          <h2 className="font-gilroy">
+          <h2 className="font-gilroy py-4 mx-auto font-semibold">
             Don't have an account?{" "}
             <a href="/#" className="text-blue-600">
               Sign up
             </a>
           </h2>
-          <div>
-            <h3 className="font-gilroy">
+          <div className="">
+            <h3 className="font-gilroy text-sm text-slate-400 text-center">
               By signing in with an account, you agree to
             </h3>
             <h2 className="font-gilroy">
-              Scissor's Terms of Service, Privacy Policy and Acceptable Use
+              <span className="text-slate-400"> Scissor's</span> Terms of
+              Service, Privacy Policy{" "}
+              <span className="text-slate-400"> and </span> Acceptable Use
               Policy.
             </h2>
           </div>
