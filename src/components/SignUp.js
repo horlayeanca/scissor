f089439d2d793ae91721fb0a8cc65ed75657f542
horlayeanca <adelodunfaruq@gmail.com>
2023-06-12 21:39:23 +0100

    completed SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,26 +1,72 @@
 import React from "react";
 import { ReactComponent as Google } from "../assets/Google.svg";
 import { ReactComponent as Apple } from "../assets/Apple.svg";
+import { ReactComponent as VectorXX } from "../assets/VectorXX.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 function SignUp() {
   return (
-    <div>
-      <form action="">
-        <h3>Sign up with:</h3>
-        <button className="bg-blue-600 py-2 px-5 flex items-center rounded">
-          <Google className="w-4 h-4 flex" /> &nbsp; Google
-        </button>
-        <button className="bg-blue-600 py-2 px-5 flex items-center rounded ml-4">
-          <Apple className="w-4 h-4 flex" />
-          &nbsp; Apple
-        </button>
-        <input type="text" placeholder="Username" />
-        <input type="email" placeholder="Email address" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm password" />
-        <button className="bg-blue-600 py-3 text-sm font-semibold rounded-full font-gilroy text-white hover:bg-blue-500">
-          Sign up
-        </button>
+    <div className="flex justify-center items-center py-10">
+      <form action="" className="flex flex-col">
+        <h3 className="text-center p-3 font-gilroy text-base text-[#5C6F7F]">
+          Sign up with:
+        </h3>
+        <div className="flex justify-center">
+          <button className="bg-blue-600 py-2 px-5 font-gilroy text-sm text-white flex items-center rounded">
+            <Google className="w-4 h-4 flex" /> &nbsp; Google
+          </button>
+          <button className="bg-blue-600 py-2 px-5 font-gilroy text-sm text-white flex items-center rounded ml-4">
+            <Apple className="w-4 h-4 flex" />
+            &nbsp; Apple
+          </button>
+        </div>
+        <div className="flex flex-row justify-center items-center py-3">
+          <VectorXX /> <h2>&nbsp; Or &nbsp;</h2>
+          <VectorXX />
+        </div>
+        <div className="grid gap-4">
+          <input
+            type="text"
+            placeholder="Username"
+            className="border-2 border-blue-600 py-2 px-4 rounded-lg focus:outline-none"
+          />
+          <input
+            type="email"
+            placeholder="Email address"
+            className="border-2 border-blue-600 py-2 px-4 rounded-lg focus:outline-none"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="border-2 border-blue-600 py-2 px-4 rounded-lg focus:outline-none"
+          />
+          <input
+            type="password"
+            placeholder="Confirm password"
+            className="border-2 border-blue-600 py-2 px-4 rounded-lg focus:outline-none"
+          />
+          <FontAwesomeIcon
+            icon={faEyeSlash}
+            className="absolute right-[450px] mt-[195px] cursor-pointer hidden"
+          />
+          <FontAwesomeIcon
+            icon={faEye}
+            className="absolute right-[450px] mt-[195px] cursor-pointer"
+          />
+          <FontAwesomeIcon
+            icon={faEyeSlash}
+            className="absolute right-[450px] mt-[136px] cursor-pointer hidden"
+          />
+          <FontAwesomeIcon
+            icon={faEye}
+            className="absolute right-[450px] mt-[136px] cursor-pointer"
+          />
+          <button className="bg-blue-600 mt-5 py-3 text-sm font-semibold rounded-full font-gilroy text-white hover:bg-blue-500">
+            Sign up
+          </button>
+        </div>
         <h2 className="font-gilroy py-4 mx-auto font-semibold">
           Already have an account?{" "}
           <a href="/#" className="text-blue-600">
